e32c7a5652b5d6f8e77a4606cc833853bdd1c649
SamMachuca <samuelandresmachuca@gmail.com>
2018-04-16 19:58:06 -0600

    condigo nuevo

--- 2Parcial_Practico_ADC_nodejs/index.js
+++ 2Parcial_Practico_ADC_nodejs/index.js
@@ -2,18 +2,23 @@ const TelegramBot = require('node-telegram-bot-api');
 
  const token = '556940789:AAFFmJyZXbGU-9HBHg632JQtXdxHtY_1LyQ';
 
+ var miSerial = new serialport("COM3", {
+   baudRate: 9600,
+   autoOpen: true
+ });
+
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/numero (.+)/, (msg, match) => {
 
   const chatId = msg.chat.id;
   const resp = match[1];
+  miSerial.setEncoding('utf8');
 
-  bot.sendMessage(chatId, resp);
+  miSerial.on('data', function(data){
+  //bot.sendMessage(chatId, resp);
 });
+var numeromax=0;
+var numeromenor=0;
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Received your message');
 });
